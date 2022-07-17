56dc13689198885e3317c2b6e4acaacf432fa280
imjoseangel <josea.munoz@gmail.com>
2022-07-17 20:14:58 +0200

    add role

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ locals {
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
-  count = local.create && var.create_iam_role ? 1 : 0
+  count = var.create && var.create_iam_role ? 1 : 0
 
   statement {
     sid     = "EKSClusterAssumeRole"
@@ -24,4 +24,19 @@ data "aws_iam_policy_document" "assume_role_policy" {
       identifiers = ["eks.${local.dns_suffix}"]
     }
   }
+}
+
+resource "aws_iam_role" "this" {
+  count = local.create_iam_role ? 1 : 0
+
+  name        = var.iam_role_use_name_prefix ? null : local.iam_role_name
+  name_prefix = var.iam_role_use_name_prefix ? "${local.iam_role_name}${var.prefix_separator}" : null
+  path        = var.iam_role_path
+  description = var.iam_role_description
+
+  assume_role_policy    = data.aws_iam_policy_document.assume_role_policy[0].json
+  permissions_boundary  = var.iam_role_permissions_boundary
+  force_detach_policies = true
+
+  tags = merge(var.tags, var.iam_role_tags)
 }
