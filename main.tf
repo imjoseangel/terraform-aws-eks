c40e3692876345411f9613bab48bb73f0065740a
imjoseangel <josea.munoz@gmail.com>
2022-07-17 20:08:49 +0200

    add basic vars

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
 data "aws_partition" "current" {}
+
+################################################################################
+# IAM Role
+################################################################################
+
+locals {
+  create_iam_role   = var.create && var.create_iam_role
+  iam_role_name     = coalesce(var.iam_role_name, "${var.cluster_name}-cluster")
+  policy_arn_prefix = "arn:${data.aws_partition.current.partition}:iam::aws:policy"
+
+  dns_suffix = data.aws_partition.current.dns_suffix
+}
+
+output "dns_suffix" {
+  value = local.dns_suffix
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+
+variable "create" {
+  description = "Controls if EKS resources should be created (affects nearly all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "prefix_separator" {
+  description = "The separator to use between the prefix and the generated timestamp for resource names"
+  type        = string
+  default     = "-"
+}
+
+################################################################################
+# Cluster
+################################################################################
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = ""
+}
+
+################################################################################
+# Cluster IAM Role
+################################################################################
+
+variable "create_iam_role" {
+  description = "Determines whether a an IAM role is created or to use an existing IAM role"
+  type        = bool
+  default     = true
+}
+
+variable "iam_role_arn" {
+  description = "Existing IAM role ARN for the cluster. Required if `create_iam_role` is set to `false`"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_name" {
+  description = "Name to use on IAM role created"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_use_name_prefix" {
+  description = "Determines whether the IAM role name (`iam_role_name`) is used as a prefix"
+  type        = bool
+  default     = true
+}
+
+variable "iam_role_path" {
+  description = "Cluster IAM role path"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_description" {
+  description = "Description of the role"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_permissions_boundary" {
+  description = "ARN of the policy that is used to set the permissions boundary for the IAM role"
+  type        = string
+  default     = null
+}
+
+variable "iam_role_additional_policies" {
+  description = "Additional policies to be added to the IAM role"
+  type        = list(string)
+  default     = []
+}
+
+variable "iam_role_tags" {
+  description = "A map of additional tags to add to the IAM role created"
+  type        = map(string)
+  default     = {}
+}
